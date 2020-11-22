f3d97d2d14156470c1d5d112731089eca76fb19c
zhuhe <war3zhuhe@gmail.com>
2020-11-22 23:05:41 +0800

    增加 DrawerLayout。实现全屏左右滑动。

---
+++ ZHtouchs/src/main/java/com/github/zhtouchs/Utils/DrawerLayoutUtil.java
@@ -0,0 +1,62 @@
+package com.github.zhtouchs.Utils;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.view.Gravity;
+
+import androidx.annotation.NonNull;
+import androidx.customview.widget.ViewDragHelper;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import java.lang.reflect.Field;
+
+public class DrawerLayoutUtil {
+    private static final String TAG = "DrawerLayoutUtil";
+
+    private DrawerLayoutUtil() {
+    }
+
+    public static void setDrawerLeftEdgeSize(@NonNull Activity activity, @NonNull DrawerLayout drawerLayout, double displayWidthPercentage) {
+        setDrawerLeftEdgeSize(activity, drawerLayout, displayWidthPercentage, Gravity.LEFT);
+        setDrawerLeftEdgeSize(activity, drawerLayout, displayWidthPercentage, Gravity.RIGHT);
+    }
+
+    private static void setDrawerLeftEdgeSize(@NonNull Activity activity, @NonNull DrawerLayout drawerLayout, double displayWidthPercentage, int gravityDic) {
+        try {
+            String draggerFieldName;
+            String leftCallbackFieldName;
+            if (gravityDic == Gravity.LEFT) {
+                draggerFieldName = "mLeftDragger";
+                leftCallbackFieldName = "mLeftCallback";
+            } else if (gravityDic == Gravity.RIGHT) {
+                draggerFieldName = "mRightDragger";
+                leftCallbackFieldName = "mRightCallback";
+            } else {
+                throw new IllegalArgumentException("gravityDic must be Gravity.LEFT or Gravity.RIGHT");
+            }
+            Field declaredField = drawerLayout.getClass().getDeclaredField(draggerFieldName);
+            declaredField.setAccessible(true);
+            ViewDragHelper leftDragger = (ViewDragHelper) declaredField.get(drawerLayout);
+            // find edgesize and set is accessible
+            Field edgeSizeField = leftDragger.getClass().getDeclaredField("mDefaultEdgeSize");
+            edgeSizeField.setAccessible(true);
+            int edgeSize = edgeSizeField.getInt(leftDragger);
+            Point displaySize = new Point();
+            activity.getWindowManager().getDefaultDisplay().getSize(displaySize);
+            edgeSizeField.setInt(leftDragger, Math.max(edgeSize, (int) (displaySize.x * displayWidthPercentage)));
+            Field leftCallbackField = drawerLayout.getClass().getDeclaredField(leftCallbackFieldName);
+            leftCallbackField.setAccessible(true);
+            ViewDragHelper.Callback callback = (ViewDragHelper.Callback) leftCallbackField.get(drawerLayout);
+            Field peekRunnableField = callback.getClass().getDeclaredField("mPeekRunnable");
+            peekRunnableField.setAccessible(true);
+            Runnable nullRunnable = new Runnable() {
+                @Override
+                public void run() {
+                }
+            };
+            peekRunnableField.set(callback, nullRunnable);
+        } catch (IllegalAccessException | NoSuchFieldException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- drawer/src/main/java/com/example/drawer/MainActivity.java
+++ drawer/src/main/java/com/example/drawer/MainActivity.java
@@ -1,30 +1,33 @@
 package com.example.drawer;
 
 import android.os.Bundle;
-import android.view.View;
 import android.view.Menu;
+import android.view.View;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import com.google.android.material.navigation.NavigationView;
-
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
-import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.navigation.ui.AppBarConfiguration;
+
+import com.github.zhtouchs.ui.drawerlayout.DrawerLayout;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "MainActivity";
+
     private AppBarConfiguration mAppBarConfiguration;
+
+    private DrawerLayout drawer;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -34,17 +37,22 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
-                .setDrawerLayout(drawer)
-                .build();
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
-        NavigationUI.setupWithNavController(navigationView, navController);
+//        mAppBarConfiguration = new AppBarConfiguration.Builder(
+//                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+//                .setDrawerLayout(drawer)
+//                .build();
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+//        NavigationUI.setupWithNavController(navigationView, navController);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
     }
 
     @Override
@@ -54,10 +62,12 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onSupportNavigateUp() {
-        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
-                || super.onSupportNavigateUp();
-    }
+//    @Override
+//    public boolean onSupportNavigateUp() {
+//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
+//        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+//                || super.onSupportNavigateUp();
+//    }
+
+
 }
