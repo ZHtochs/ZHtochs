d560bb80bee6631163281911de15318cbe40d7d7
zhuhe <war3zhuhe@gmail.com>
2021-03-24 23:18:09 +0800

    add hook AMS

--- app/src/main/java/com/example/app/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/app/ui/home/HomeFragment.java
@@ -1,7 +1,10 @@
 package com.example.app.ui.home;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,9 +16,12 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import com.example.app.Activity2;
 import com.example.app.MobileNavigationArgs;
 import com.example.app.R;
+import com.example.app.hook.HookHelper;
 import com.github.zhtouchs.Utils.ZHLog;
+import com.github.zhtouchs.ZHActivityManager;
 
 public class HomeFragment extends Fragment implements View.OnClickListener {
     private static final String TAG = "HomeFragment";
@@ -35,23 +41,35 @@ public class HomeFragment extends Fragment implements View.OnClickListener {
         getid.setOnClickListener(this);
         removeid = root.findViewById(R.id.removeid);
         removeid.setOnClickListener(this);
+        root.findViewById(R.id.removeid2).setOnClickListener(this);
         return root;
     }
 
 
     @Override
     public void onClick(View v) {
+        ZHLog.d(TAG, "onClick " + v.getId());
+
         switch (v.getId()) {
             case R.id.getid:
-                ZHLog.d(TAG,"getid");
-                Intent intent = new Intent();
-                intent.setAction(CODELABS_ACTION);
-                intent.putExtra("method", "onMessageSent");
-
-                getActivity().sendBroadcast(intent);
+                String str = Settings.System.getString(ZHActivityManager.INSTANCE.getContext()
+                                .getContentResolver(),
+                        Settings.System.NEXT_ALARM_FORMATTED);
+                ZHLog.d(TAG, str);
+                AlarmManager alarmManager = (AlarmManager) requireActivity().getSystemService(Context.ALARM_SERVICE);
+                AlarmManager.AlarmClockInfo info = alarmManager.getNextAlarmClock();
+                alarmManager.setAlarmClock(new AlarmManager.AlarmClockInfo(1616369400000L + 86400000L, null)
+                        , null);
                 break;
             case R.id.removeid:
-
+                try {
+                    HookHelper.hookAMS();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                break;
+            case R.id.removeid2:
+                getActivity().startActivity(new Intent(getActivity(), Activity2.class));
                 break;
         }
     }
