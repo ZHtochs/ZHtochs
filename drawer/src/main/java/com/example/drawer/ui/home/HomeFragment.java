5d6165eb73418aa36206c28587472866114edfd6
ZHtouchs <war3zhuhe@gmail.com>
2022-01-22 00:45:49 +0800

    可展开收起linearLayout

--- drawer/src/main/java/com/example/drawer/ui/home/HomeFragment.java
+++ drawer/src/main/java/com/example/drawer/ui/home/HomeFragment.java
@@ -1,35 +1,30 @@
 package com.example.drawer.ui.home;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.slidingpanelayout.widget.SlidingPaneLayout;
-import com.example.domain.FeedDatabase;
-import com.example.domain.FeedEntry;
-import com.example.domain.FeedReaderContract;
 import com.example.domain.FeedReaderDbHelper;
 import com.example.drawer.R;
-import com.example.okhttp.OkHttpTest;
+import com.example.drawer.databinding.FragmentHomeBinding;
+import com.example.drawer.databinding.ItemTextOnlyBinding;
+import com.example.drawer.ui.gallery.beans.ItemBean;
 import com.github.zhtouchs.Utils.ZHLog;
-import com.github.zhtouchs.Utils.ZHThreadPool;
-import com.github.zhtouchs.ZHActivityManager;
 import com.github.zhtouchs.activity.BaseFragment;
-import okhttp3.Response;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class HomeFragment extends BaseFragment implements View.OnClickListener {
     private static final String TAG = "HomeFragment";
@@ -37,11 +32,18 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
     private final String urlString = "https://pic1.zhimg.com/80/v2-15b2a9a8a8ac38d7ddd47fe9b792232b_720w.jpg?source=1940ef5c";
     private HomeViewModel homeViewModel;
 
+    private ExecutorService service = Executors.newSingleThreadExecutor();
+
+    FragmentHomeBinding fragmentHomeBinding;
+
     FeedReaderDbHelper dbHelper;
+
+    private boolean isExpanded = false;
+
+    Handler looper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        ZHLog.d(TAG,"onCreateView");
         homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
@@ -51,7 +53,8 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
             }
         });
         homeViewModel.setmText("Sadasdada");
-        View root = inflater.inflate(R.layout.fragment_home, container, false);
+        fragmentHomeBinding = FragmentHomeBinding.inflate(inflater, container, false);
+        View root = fragmentHomeBinding.getRoot();
         root.findViewById(R.id.button_get).setOnClickListener(this);
         root.findViewById(R.id.button_post).setOnClickListener(this);
         root.findViewById(R.id.button_upload_file).setOnClickListener(this);
@@ -60,98 +63,51 @@ public class HomeFragment extends BaseFragment implements View.OnClickListener {
         slidingPaneLayout.setLayoutDirection(View.LAYOUT_DIRECTION_RTL);
         slidingPaneLayout.setParallaxDistance(getResources().getDimensionPixelOffset(R.dimen.dp_40));
         dbHelper = new FeedReaderDbHelper(getActivity());
-
+        ArrayList<View> arrayList = new ArrayList<>();
+        for (int i = 0; i < 15; i++) {
+            ItemBean bean = new ItemBean("6", "" + new Random().nextFloat());
+            ItemTextOnlyBinding itemTextOnlyBinding
+                    = ItemTextOnlyBinding.inflate(getLayoutInflater(), fragmentHomeBinding.testLinearLayout, false);
+            itemTextOnlyBinding.setTextViewEntry(bean);
+            arrayList.add(itemTextOnlyBinding.getRoot());
+        }
+        fragmentHomeBinding.testLinearLayout.setViewList(arrayList);
         return root;
     }
 
     @Override
     public void onCreate(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ZHLog.d(TAG,"onCreate");
     }
 
     @Override
     public void onSaveInstanceState(@NonNull @NotNull Bundle outState) {
         super.onSaveInstanceState(outState);
-        ZHLog.d(TAG,"onSaveInstanceState");
     }
 
     @Override
     public void onViewStateRestored(@Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
-        ZHLog.d(TAG,"onViewStateRestored");
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.button_get:
-                ZHThreadPool.INSTANCE.execute(TAG, new Runnable() {
-                    @Override
-                    public void run() {
-                        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-                        for (int i = 0; i < 10; i++) {
-                            ContentValues values = new ContentValues();
-                            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_TITLE, "title" + i);
-                            values.put(FeedReaderContract.FeedEntry.COLUMN_NAME_SUBTITLE, "subtitle" + i);
-
-// Insert the new row, returning the primary key value of the new row
-                            long newRowId = db.insert(FeedReaderContract.FeedEntry.TABLE_NAME, null, values);
-                        }
-                    }
-                });
+                fragmentHomeBinding.testLinearLayout.switchState();
                 break;
             case R.id.button_post:
-                ZHThreadPool.INSTANCE.execute(TAG, new Runnable() {
-                    @Override
-                    public void run() {
-                        FeedDatabase feedDatabase = FeedDatabase.getInstance(getContext());
-                        List<FeedEntry> allFeeds = feedDatabase.getFeedDao().getAllFeeds();
-                        ZHLog.d(TAG, allFeeds.size());
-                    }
-                });
-
                 break;
             case R.id.button_upload_file:
-                ZHThreadPool.INSTANCE.execute(TAG, new Runnable() {
-                    @Override
-                    public void run() {
-                        FeedDatabase feedDatabase = FeedDatabase.getInstance(getContext());
-                        List<FeedEntry> list = new ArrayList<>();
-                        FeedEntry feedEntry = new FeedEntry();
-                        feedEntry.setSubTitle("324234");
-                        feedEntry.setTitle("dfsfgsdfgdfgfd");
-                        list.add(feedEntry);
-                        feedDatabase.getFeedDao().insertFeed(feedEntry);
-                    }
-                });
+                getActivity().finish();
                 break;
 
             case R.id.button_download_file:
-                OkHttpTest.okGet("https://fgo.wiki/images/thumb/9/99/%E8%8A%A6%E5%B1%8B%E9%81%93%E6%BB%A12-KMurasaki.jpeg/750px-%E8%8A%A6%E5%B1%8B%E9%81%93%E6%BB%A12-KMurasaki.jpeg",
-                        new OkHttpTest.OkHttpCallBack() {
-                            @Override
-                            public void onResponse(@NotNull Response response) {
-                                File file1 = new File(ZHActivityManager.INSTANCE.getContext().getDataDir().getPath() + "/downloadFile.jpg");
-                                if (file1.exists()) {
-                                    file1.delete();
-                                }
-                                try (InputStream inputStream = response.body().byteStream();
-                                     FileOutputStream out = new FileOutputStream(file1)) {
-                                    byte[] buf = new byte[2048];
-                                    long sum = 0;
-                                    int len = 0;
-
-                                    while ((len = inputStream.read(buf)) != -1) {
-                                        out.write(buf, 0, len);
-                                        sum += len;
-                                    }
-                                } catch (IOException e) {
-
-                                }
-                            }
-                        });
+                ZHLog.d(TAG, "onClick");
+                Animation animation = AnimationUtils.loadAnimation(getContext(), R.anim.view_anim);
+                animation.setFillAfter(true);
+                animation.setFillBefore(false);
+                fragmentHomeBinding.buttonDownloadFile.startAnimation(animation);
                 break;
             default:
                 break;
---
+++ drawer/src/main/java/com/example/drawer/ui/view/ExpandableLayout.java
@@ -0,0 +1,175 @@
+package com.example.drawer.ui.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+import android.widget.LinearLayout;
+import androidx.annotation.Nullable;
+import androidx.interpolator.view.animation.FastOutLinearInInterpolator;
+import com.github.zhtouchs.Utils.ZHLog;
+
+import java.util.List;
+
+/**
+ * @program: MyDemo
+ * @author: zhuhe
+ * @create: 2022-01-21 23:26
+ **/
+public class ExpandableLayout extends LinearLayout implements Animation.AnimationListener {
+    private static final String TAG = "ExpandableLayout";
+
+    public static final int MIN_COUNT = 5;
+
+    private boolean isAnimating = false;
+
+    private boolean expandable = false;
+
+    private boolean collapsed = false;
+
+    private OnExpandListener expandListener;
+
+    private int totalSize;
+
+    private int itemHeight;
+
+    private int totalHeight;
+
+    private boolean needRelayout;
+
+    public ExpandableLayout(Context context) {
+        this(context, null);
+    }
+
+    public ExpandableLayout(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ExpandableLayout(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        this(context, attrs, defStyleAttr, 0);
+    }
+
+    public ExpandableLayout(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        super.setOrientation(VERTICAL);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        return isAnimating;
+    }
+
+    @Override
+    public void setOrientation(int orientation) {
+        if (orientation != VERTICAL) {
+            throw new IllegalStateException("orientation must be VERTICAL");
+        }
+        super.setOrientation(orientation);
+    }
+
+    public void setViewList(@Nullable List<View> viewList) {
+        if (viewList == null || viewList.isEmpty()) {
+            setVisibility(GONE);
+            return;
+        }
+        needRelayout = true;
+        totalSize = viewList.size();
+        for (View view : viewList) {
+            addView(view);
+        }
+    }
+
+    public void switchState() {
+        if (!expandable || isAnimating) {
+            return;
+        }
+        ExpandAbleAnimation animation;
+        ZHLog.d(TAG, "switchState " + collapsed);
+        if (collapsed) {
+            animation = new ExpandAbleAnimation(MIN_COUNT * itemHeight, totalHeight);
+        } else {
+            animation = new ExpandAbleAnimation(totalHeight, MIN_COUNT * itemHeight);
+        }
+        animation.setAnimationListener(this);
+        startAnimation(animation);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        if (totalSize == 0) {
+            setVisibility(GONE);
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        if (!needRelayout) {
+            super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+            return;
+        }
+        needRelayout = false;
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        totalHeight = getMeasuredHeight();
+        itemHeight = totalHeight / totalSize;
+        expandable = totalSize > MIN_COUNT;
+        ZHLog.d(TAG, "itemHeight " + itemHeight);
+        ZHLog.d(TAG, "totalHeight " + totalHeight);
+
+        int width = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        int actualHeight;
+        if (expandable) {
+            actualHeight = MeasureSpec.makeMeasureSpec(itemHeight * MIN_COUNT, MeasureSpec.EXACTLY);
+        } else {
+            actualHeight = MeasureSpec.makeMeasureSpec(Integer.MAX_VALUE >> 2, MeasureSpec.AT_MOST);
+        }
+        super.onMeasure(width, actualHeight);
+    }
+
+    @Override
+    public void onAnimationStart(Animation animation) {
+        isAnimating = true;
+    }
+
+    @Override
+    public void onAnimationEnd(Animation animation) {
+        isAnimating = false;
+        collapsed = !collapsed;
+        ZHLog.d(TAG, "collapsed " + collapsed);
+        if (expandListener != null) {
+            expandListener.onStateChanged(this, !collapsed);
+        }
+    }
+
+    @Override
+    public void onAnimationRepeat(Animation animation) {
+
+    }
+
+    public interface OnExpandListener {
+        public void onStateChanged(View view, boolean isExpand);
+    }
+
+    private class ExpandAbleAnimation extends Animation {
+
+        final int startHeight;
+        final int endHeight;
+
+        public ExpandAbleAnimation(int startHeight, int endHeight) {
+            this.startHeight = startHeight;
+            this.endHeight = endHeight;
+            ZHLog.d(TAG, "startHeight " + startHeight + " endHeight " + endHeight);
+            setDuration(300);
+            setInterpolator(new FastOutLinearInInterpolator());
+            ExpandableLayout.this.getLayoutParams().height = startHeight;
+        }
+
+        @Override
+        protected void applyTransformation(float interpolatedTime, Transformation transformation) {
+            int height = startHeight + (int) ((endHeight - startHeight) * interpolatedTime);
+            ExpandableLayout.this.getLayoutParams().height = height;
+            ExpandableLayout.this.requestLayout();
+        }
+    }
+}
